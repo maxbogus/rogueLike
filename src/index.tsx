5fcba23b883bccd5136942f03668d4a393dd5963
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2023-07-28 13:30:06 +0300

    added menu

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client'
+import React, {useState} from 'react';
+import {createRoot} from 'react-dom/client';
 
-const App = () => <div>
-    Hello world
-</div>
+const menuItems = ['Game', 'Results', 'Help'];
+
+const App = () => {
+    const [selectedState, updateState] = useState<string>(menuItems[0]);
+    const renderMenu = () => <>
+        <ul>
+            {menuItems.map(item => <li key={item} onClick={() => {
+                updateState(item);
+            }}>{item}</li>)}
+        </ul>
+    </>;
+
+    const renderResults = () => <>
+        <div onClick={() => updateState('Menu')}>Back</div>
+        <div>Results</div>
+    </>;
+
+    const renderGame = () => <>
+        <div onClick={() => updateState('Menu')}>Back</div>
+        <div>Game</div>
+    </>;
+
+    const renderHelp = () => <>
+        <div onClick={() => updateState('Menu')}>Back</div>
+        <div>Help</div>
+    </>;
+
+    const showState = (currentState: string) => {
+        switch (currentState) {
+            case 'Help': {
+                return renderHelp();
+            }
+            case 'Game': {
+                return renderGame();
+            }
+            case 'Results': {
+                return renderResults();
+            }
+            default: {
+                return renderMenu();
+            }
+        }
+    }
+
+    return (<div>
+    <h1>Roguelike</h1>
+        {showState(selectedState)}
+</div>)};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(<App/>);
