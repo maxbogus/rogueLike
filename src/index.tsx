d07e02f6c5321b89e7eaaf1b7f54e0dac51e8c8c
Max Boguslavskiy <mboguslavskiy@aligntech.com>
2023-07-28 20:08:22 +0300

    added results screen

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,18 @@ import {createRoot} from 'react-dom/client';
 
 const menuItems = ['Game', 'Results', 'Help'];
 
+interface GameResult {
+    isWon: boolean;
+    slainBy?: string;
+    floor: number;
+    date: Date;
+}
+
+const showResult = ({isWon, slainBy, floor}: GameResult): string => `${isWon ? `Player won` : `Player slayed by ${slainBy}`}  on floor ${floor}.`;
+
 const App = () => {
     const [selectedState, updateState] = useState<string>(menuItems[0]);
+    const [results, updateResults] = useState<GameResult[]>([]);
     const renderMenu = () => <>
         <ul>
             {menuItems.map(item => <li key={item} onClick={() => {
@@ -15,12 +25,17 @@ const App = () => {
 
     const renderResults = () => <>
         <div onClick={() => updateState('Menu')}>Back</div>
-        <div>Results</div>
+        <div>Results:</div>
+        <ul>
+            {results.map((result, index) => <li key={index}>{`${index}. ${showResult(result)}`}</li>)}
+        </ul>
     </>;
 
     const renderGame = () => <>
         <div onClick={() => updateState('Menu')}>Back</div>
         <div>Game</div>
+        <div onClick={() => {updateResults([...results, {isWon: false, slainBy: 'ghoul', floor: 2, date: new Date()}])}}>Lose</div>
+        <div onClick={() => {updateResults([...results, {isWon: true, floor: 20, date: new Date()}])}}>Win</div>
     </>;
 
     const renderHelp = () => <>
